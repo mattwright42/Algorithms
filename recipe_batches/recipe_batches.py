11c50b4fadb1c8f92a8ce6030988bd3cb42bc6a6
Matthew Wright <matthewbwright@gmail.com>
2019-02-20 15:16:18 -0700

    completes recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
 
 import math
+import operator
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # get the max value from ingredients.items
+    batches = max(ingredients.items(), key=operator.itemgetter(1))[1]
+  # loop through the recipes dict
+    for rkey in recipe.items():
+        # if the key from the recipe does NOT appear in ingredietns, batches = 0
+        if rkey[0] not in ingredients.keys():
+            batches = 0
+        # loop through ingredients dict
+        for ikey in ingredients.items():
+          # if the key of ingredients matches the key of recipe
+            if ikey[0] == rkey[0]:
+              # check whether we have more ingredients than is called for in the recipe
+                if ikey[1] > rkey[1]:
+                    # if so, check whether the division is smaller than the current total number of batches
+                    if (ikey[1] // rkey[1]) < batches:
+                        # if it is lower, then we decrease the total number of batches can be made
+                        batches = (ikey[1] // rkey[1])
+
+    # return total number of batches that can be made
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+                    # Change the entries of these dictionaries to test
+                    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 51, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
